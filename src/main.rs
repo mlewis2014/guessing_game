1fe9a264915ded299a18c048ff9c45c2f7fcc8be
Markko <bikermark58@gmail.com>
2019-07-20 13:18:40 -0500

    Completed and tested

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+// guessing game
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    println!("Guess a number between 1 and 100!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop{
+	    println!("Enter your best guess now: ");
+
+	 		let mut guess = String::new();
+
+	 		io::stdin().read_line(&mut guess)
+	    		.expect("Failed to read line");
+
+	    let guess: u32 = match guess.trim().parse() {
+	    	Ok(num) => num, 
+	    	Err(_) => continue, 
+	    
+	    };
+
+	    println!("You guessed: {}", guess);
+
+	    match guess.cmp(&secret_number) {
+	    		Ordering::Less => println!("Too small!"),
+	        Ordering::Greater => println!("Too big!"),
+	        Ordering::Equal => { 
+	        	println!("You win!");
+	        	break;
+	        }
+	    }
+		}
+
 }
+
